0618e94720580603f4a15a6d5c0b38da7e76aa34
charmingchm <842721197@qq.com>
2020-07-14 15:32:11 +0800

    testing

---
+++ demo/demo27.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// 测试
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
+func main() {
+	//r := setupRouter()
+	//r.Run()
+	t := new(testing.T)
+	TestPingRoute(t)
+}
+
+func TestPingRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}

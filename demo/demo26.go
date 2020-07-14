cb8516131703841fdc8f2d208b369a22b6f2fa6e
charmingchm <842721197@qq.com>
2020-07-14 15:24:22 +0800

    Set and get a cookie

---
+++ demo/demo26.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+// 设置并获取cookie
+func main() {
+	router := gin.Default()
+
+	router.GET("/cookie", func(c *gin.Context) {
+
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+		}
+
+		fmt.Printf("Cookie value: %s \n", cookie)
+	})
+
+	router.Run()
+}
